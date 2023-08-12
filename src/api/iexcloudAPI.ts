e6ded2353d97a55d1d5e1769b090708f3d25163b
ToniNov <antoni.novik@yandex.ru>
2023-08-12 16:40:47 +0300

    fix: readme

--- src/api/iexcloudAPI.ts
+++ src/api/iexcloudAPI.ts
@@ -2,21 +2,18 @@ import axios from 'axios';
 
 import { StockData } from '../types';
 
+const BASE_URL = 'https://cloud.iexapis.com';
+const token = process.env.REACT_APP_API_TOKEN_IEXCLOUD;
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  params: { token },
+});
+
 export const iexcloudAPI = {
   async fetchData(): Promise<StockData[]> {
-    const token = process.env.REACT_APP_API_TOKEN_IEXCLOUD;
+    const response = await instance.get('/stable/tops');
 
-    try {
-      const response = await axios.get('https://cloud.iexapis.com/stable/tops', {
-        params: {
-          token,
-        },
-      });
-
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      throw error;
-    }
+    return response.data;
   },
 };
--- src/components/Table/PageNumbers.tsx
+++ src/components/Table/PageNumbers.tsx
@@ -22,14 +22,14 @@ export const PageNumbers: React.FC<PropsType> = ({
       {displayedPages.map((pageNumber) => (
         <li
           key={pageNumber}
-          className={`relative inline-flex items-center justify-center px-2 py-2 w-12 border border-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-50 ${
+          className={`relative inline-flex items-center justify-center border border-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-50 ${
             pageNumber === currentPage ? 'bg-green' : 'bg-white'
           }`}
         >
           <button
             onClick={() => handlePageChange(pageNumber)}
             type="button"
-            className="page-link"
+            className="page-link w-12 px-2 py-2"
           >
             {pageNumber}
           </button>
